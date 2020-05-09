e982fa813e0533c50ddedb9f04abb8ce7c7ca55b
MisaelMa <amisael.amir.misael@gmail.com>
2020-05-09 16:12:27 -0500

    se agrego el lector de qr

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,7 @@ module.exports = {
   rules: {
     // override/add rules settings here, such as:
     // 'vue/no-unused-vars': 'error'
-    "operator-linebreak": "off"
+    'operator-linebreak': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
   },
 };
